2dddceb71c17515ab1cf1b2855ee94279b871860
sc30gsw <haidoushi875@gmail.com>
2021-01-25 19:57:27 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_103155) do
+ActiveRecord::Schema.define(version: 2021_01_25_105537) do
+
+  create_table "intros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "website"
+    t.text "profile"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_intros_on_user_id"
+  end
 
   create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -35,5 +46,6 @@ ActiveRecord::Schema.define(version: 2021_01_25_103155) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "intros", "users"
   add_foreign_key "notes", "users"
 end
