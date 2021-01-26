1a6ef455f42ede8c6098ced49dd4b000f666e8c5
sc30gsw <haidoushi875@gmail.com>
2021-01-26 12:18:46 +0900

    testの準備

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :note do
-    
+    title { Faker::Lorem.sentence }
+    text { Faker::Lorem.sentence }
+    plan { Faker::Lorem.sentence }
+
+    association :user
+
+    after(:build) do |note|
+      note.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/factories/tags.rb
+++ spec/factories/tags.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :tag do
-    
+    name { 'test' }
   end
 end
